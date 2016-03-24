f961ca70fb5e15ecaf5badebec79634054a73968
Jonathan Muschalle <j.muschalle@gmail.com>
2016-03-24 18:43:34 -0400

    RESV001 - Creation de la migration

---
+++ resto/database/migrations/2016_03_24_223141_booking_customer.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class BookingCustomer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('email');
+            $table->string('phone');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('reserved_at');
+            $table->dateTime('nb_invites');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->integer('customer_id')->unsigned();
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservations');
+        Schema::drop('customers');
+    }
+}
