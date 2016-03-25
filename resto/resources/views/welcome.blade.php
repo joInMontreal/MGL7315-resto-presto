ddfd5b60852617cb19fc50329517d1f91ce23a7f
Jonathan Muschalle <j.muschalle@gmail.com>
2016-03-25 00:25:00 -0400

    [divers] Creation du layout de base

--- resto/resources/views/welcome.blade.php
+++ resto/resources/views/welcome.blade.php
@@ -1,49 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Resto Presto</title>
+@extends('layouts.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('title', 'Acceuil')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+@section('menu_home', 'class="active"')
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Resto Presto</div>
-                <h3>Les utilisateurs :</h3>
-                @foreach ($users as $user)
-                    <p>{{ $user->name }}</p>
-                @endforeach
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+    <h3>Les utilisateurs :</h3>
+    @foreach ($users as $user)
+        <p>{{ $user->name }}</p>
+    @endforeach
+@endsection
